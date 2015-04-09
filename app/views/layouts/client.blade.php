10b844d01fc4553db812d676e208b68e254e8970
OSDDQD <slowpokedme@gmail.com>
2015-04-09 17:40:38 +0500

    Add view to categories

--- app/views/layouts/client.blade.php
+++ app/views/layouts/client.blade.php
@@ -61,6 +61,20 @@
     <!-- end: Left Sidebar -->
 
     <section id="main-content" class="clearfix">
+
+        <article id="intro" class="section-wrapper clearfix" data-custom-background-img="assets/img/client/default_bg.jpg">
+            <div class="content-wrapper clearfix wow fadeInDown" data-wow-delay="0.3s">
+                <div class="col-sm-10 col-md-9 pull-right">
+
+                    <section class="feature-text">
+                        <h1>UNESCO Catalog</h1>
+                        <p>This is DEMO Content..</p>
+                    </section>
+
+                </div><!-- .col-sm-10 -->
+            </div><!-- .content-wrapper -->
+        </article><!-- .section-wrapper -->
+
         @yield('content')
     </section><!-- #main-content -->
 
